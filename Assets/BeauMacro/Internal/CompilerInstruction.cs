3fa14549aef88dd351de46167286349506b970fa
abeauchesne <abeauchesne@filamentgames.com>
2019-07-24 17:59:03 -0500

    * Initial commit

---
+++ Assets/BeauMacro/Internal/CompilerInstruction.cs
@@ -0,0 +1,26 @@
+﻿/*
+ * Copyright (C) 2017 - 2019. Filament Games, LLC. All rights reserved.
+ * Author:  Alex Beauchesne
+ * Date:    23 July 2019
+ * 
+ * File:    CompilerInstruction.cs
+ * Purpose: Instruction, with operation and optional argument.
+ */
+
+namespace BeauMacro
+{
+    /// <summary>
+    /// String macro instruction.
+    /// </summary>
+    internal struct CompilerInstruction
+    {
+        public OpCode Operation;
+        public int? IntArg;
+
+        public CompilerInstruction(OpCode inOperation, int? inArg = null)
+        {
+            Operation = inOperation;
+            IntArg = inArg;
+        }
+    }
+}
